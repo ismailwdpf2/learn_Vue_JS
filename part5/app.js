97ed3748d0c28c54eb55b38e29b0f7189bb9fa06
ismail hosen <ismailwdpf@gmail.com>
2023-08-20 19:28:03 +0600

    part 5 complete

--- part5/app.js
+++ part5/app.js
@@ -4,6 +4,20 @@ var app = Vue.createApp(
             return{
                count: 0,
             };
+        },
+        methods: {
+            // incre(){
+            //     this.count=this.count + 1;
+            // },
+            // decre(){
+            //     this.count=this.count - 1;
+            // }
+            incre(){
+                this.count ++;
+            },
+            decre(){
+                this.count --;
+            }
         }
     });
 app.mount("#app");
